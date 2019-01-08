f65dbe5ce185dae281ec4fb95034e5b6d6155160
racdale <racdale@gmail.com>
2019-01-07 17:11:32 -0800

    updating pca with prcomp

--- Code files/DataAnalyses.R
+++ Code files/DataAnalyses.R
@@ -1,15 +1,15 @@
-#This file contains the codes for the analysis we did on the project.
+######## This file contains the codes for the analysis we did on the project.
 
-#Libraries
+######## Libraries
 
 library(lme4)
 library(lmerTest)
+library(matrixStats)
 
-rt = "./ComplexCommunicationDynamics/Data files" #The path to the working directory
-setwd(rt) #Set the working directory
+rt = "./ComplexCommunicationDynamics/Data files" # The path to the working directory
+setwd(rt) # Set the working directory
 
-
-#Let's load up the datasets:
+####### Let's load up the datasets:
 
 AllDataRaw = read.table('AllDataRaw.txt', header=T, sep = '')
 
@@ -17,14 +17,14 @@ AllDataScaled = read.table('AllDataScaled.txt', header=T, sep = '')
 
 AllDataScaledInd = read.table('AllDataScaledInd.txt', header=T, sep = '')
 
-MatrixScaledInd = read.table('MatrixScaledInd.txt', header=T, sep = '') #This is the matrix (standardized per speaker) in which we performed PCA
+MatrixScaledInd = read.table('MatrixScaledInd.txt', header=T, sep = '') # This is the matrix (standardized per speaker) in which we performed PCA
 
-MatrixScaledSample = read.table('MatrixScaledSample.txt', header=T, sep = '') #This is the matrix (standardized across the sample) in which we performed PCA
+MatrixScaledSample = read.table('MatrixScaledSample.txt', header=T, sep = '') # This is the matrix (standardized across the sample) in which we performed PCA
 
 
-#Mixed effects models
-#The relevant variable names for the model are "mean.bodyw.", "sRate", "speechrate..nsyll.dur.", 
-#"articulation.rate..nsyll...phonationtime.", "f0mean", "meanIntensity"
+######## Mixed effects models
+# The relevant variable names for the model are "mean.bodyw.", "sRate", "speechrate..nsyll.dur.", 
+# "articulation.rate..nsyll...phonationtime.", "f0mean", "meanIntensity"
 
 lmeoFullRandom=lmer(meanIntensity~i+isq
                     +(1+i+isq|fl), data=AllDataScaledInd)
@@ -43,28 +43,44 @@ lmeoIntercept=lmer(meanIntensity~1
                         +(1|fl), 
                         data=AllDataScaledInd)
 
-
-
-#plotting the average trends for each DV
+######## Plotting the average trends for each DV
 avg= aggregate(AllDataRaw[,3]~i, data = AllDataRaw, mean) #The column will change depending on what variable we want to plot
 plot(avg, type='l', xlab = "Time Window",
      ylab = "Body movement", cex.lab=1.4)
 
 
 
-#Principal Component Analysis
+######## Principal Component Analysis
 
-PcaModel = princomp(ScaledDataSample) #This analyses were performed over the matrices that only included the 6 variables of interest
-                                      #They were done both on the data scaled across the sample, and the data scaled per subject.
-PcaModelSub$loadings
+### Scaled across sample
+PcaModel = prcomp(MatrixScaledSample[,1:6],scale=F,center=F) 
+PcaModel$sdev^2
+(PcaModel$sdev^2)/sum(PcaModel$sdev^2)
+PcaModel$rotation
 
-VarExp = cumsum(PcaModel$sdev^2)/sum(PcaModel$sdev^2)*100 
-VarExp
+# Reconstruct fully with svd 
+svdModel = svd(MatrixScaledSample[,1:6]) # omit t from the solution
+svdNewScores = svdModel$u %*% diag(svdModel$d)
+svdModel$v # same as rotation above
+colSds(svdNewScores)^2 # same as PcaModel$sdev^2 above
+plot(svdNewScores[,1],PcaModel$x[,1]) # equivalent
 
+### Scaled within individual speakers
+PcaModel = prcomp(MatrixScaledInd[,1:6],scale=F,center=F) # omit t from the solution
+PcaModel$sdev^2
+(PcaModel$sdev^2)/sum(PcaModel$sdev^2)
+PcaModel$rotation
 
-#Plotting PCA
+# Reconstruct fully with svd along
+svdModel = svd(MatrixScaledInd[,1:6]) # omit t from the solution
+svdNewScores = svdModel$u %*% diag(svdModel$d)
+svdModel$v # same as rotation above
+colSds(svdNewScores)^2 # same as PcaModel$sdev^2 above
+plot(svdNewScores[,1],PcaModel$x[,1]) # equivalent
 
-speakerId = rep(0,10) #creating a variable to use when plotting every subject with a different shape
+######## Plotting PCA. NB: This was removed from the final paper; this plots speaker windows in PC1 and PC2 space; original plotting used princomp (scores vs. x)
+
+speakerId = rep(0,10) # creating a variable to use when plotting every subject with a different shape
 for (i in 1:20) {
   speakerId = c(speakerId,rep(i,10))
 }
@@ -74,8 +90,7 @@ for (i in 1:300) {
   cols = c(cols,rgb(scaledDataTime$i/12,scaledDataTime$i/12,scaledDataTime$i/12))
 }
 
-
-plot(PcaModel$scores[,1],PcaModel$scores[,2],pch=speakerId, col=cols, #Plotting the data projected into the first two components
+plot(PcaModel$scores[,1],PcaModel$scores[,2],pch=speakerId, col=cols, # Plotting the data projected into the first two components
      xlab = "Component 1", ylab="Component 2", cex.lab=1.2)
 
 #Plotting the each speaker's time series for each DV
